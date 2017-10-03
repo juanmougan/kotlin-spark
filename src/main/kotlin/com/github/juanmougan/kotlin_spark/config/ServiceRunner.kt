587355b768b913081975a0c812fa2c3efd0920c0
Juan Manuel Mougan <juanmougan@gmail.com>
2017-10-02 22:00:36 -0300

    Initial setup

---
+++ src/main/kotlin/com/github/juanmougan/kotlin_spark/config/ServiceRunner.kt
@@ -0,0 +1,12 @@
+package com.github.juanmougan.kotlin_spark.config
+
+/**
+ * Main Runner of the Kotlin-Spark-Requery demo service. It makes initialization
+ * of a whole service and setup of REST API.
+ * Created by juanma on 2/10/17.
+ */
+class ServiceRunner {
+    fun run() {
+        println("Hello World!")
+    }
+}
