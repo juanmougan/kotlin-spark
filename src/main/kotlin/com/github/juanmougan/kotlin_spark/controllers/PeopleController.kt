adb432e4a33571b9cd1653580f51f39282091e5a
Juan Manuel Mougan <juanmougan@gmail.com>
2017-10-09 21:53:00 -0300

    Added GSON to have JSON serialization

--- src/main/kotlin/com/github/juanmougan/kotlin_spark/controllers/PeopleController.kt
+++ src/main/kotlin/com/github/juanmougan/kotlin_spark/controllers/PeopleController.kt
@@ -1,12 +1,16 @@
 package com.github.juanmougan.kotlin_spark.controllers
 
+import com.google.gson.Gson
 import spark.Spark.*
 
 /**
  * Created by juanma on 2/10/17.
  */
 class PeopleController {
+
+    var gson: Gson = Gson()
+
     init {
-        get("/people", { req, res -> arrayOf("Lionel Messi", "Cristiano Ronaldo", "Neymar Jr.") })
+        get("/people", { req, res -> arrayOf("Lionel Messi", "Cristiano Ronaldo", "Neymar Jr.") }, gson::toJson)
     }
 }
